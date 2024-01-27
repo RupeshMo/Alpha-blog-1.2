5a3f03f83b0605919916a1f1e9cf59645037da4a
Rupesh M <rupeshcool208@gmail.com>
2024-01-27 15:02:36 +0530

    Added home and link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,12 @@ class ArticlesController < ApplicationController
   
   # For list all articles
   def index
+    @articles = Article.all
   end 
+
+  # For showing a particular articles with reference to the id passed in a hash
+  def show
+    # First fetch the id and save it in a instance variable to make it acessible outside this class
+    @article = Article.find(params[:id]) # :id is a route parameter capturer in the 
+  end
 end
