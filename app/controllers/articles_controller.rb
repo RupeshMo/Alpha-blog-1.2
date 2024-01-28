24c9847462fda9db7ff754ae3b1b6d8386bcc2f8
Rupesh M <rupeshcool208@gmail.com>
2024-01-28 15:04:31 +0530

    Delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,44 @@ class ArticlesController < ApplicationController
     # First fetch the id and save it in a instance variable to make it acessible outside this class
     @article = Article.find(params[:id]) # :id is a route parameter capturer in the 
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.new(article_params)
+
+      if @article.save
+        redirect_to @article
+      else
+        render :edit, status: :unprocessable_entity
+      end
+  end
+
+  def destroy 
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+  def article_params 
+    params.require(:article).permit(:title, :body)
+  end
 end
